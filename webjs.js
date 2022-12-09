c59176c937fce0e0be846138c8ed4699af47dc88
Will S <114497071+MrStark7797@users.noreply.github.com>
2022-12-09 11:04:36 +0000

    WAS WORKING POS FOR BOARD TILL CODE FOR MODE BUTTTON WAS ADDED

--- webjs.js
+++ webjs.js
@@ -29,3 +29,23 @@ board.forEach(row => {
 
 //setting the board to be the correct size for the grid
 boardElement.style.setProperty("--size", gridSize)
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
